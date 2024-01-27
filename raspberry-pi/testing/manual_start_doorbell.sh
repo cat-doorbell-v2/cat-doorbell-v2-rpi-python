b5475534d9ed25e411e641fd71ea0fd414c1afcf
gamename <tennis.n.smith@gmail.com>
2024-01-27 13:37:18 -0600

    add new test. remove old

--- raspberry-pi/testing/manual_start_doorbell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export DISPLAY=:0
-export DOORBELL="${HOME}/PycharmProjects/raspberry-pi-cat-doorbell-v2/raspberry-pi/doorbell.py"
-nohup python3 "${DOORBELL}" > /tmp/doorbell.log 2>&1 &
