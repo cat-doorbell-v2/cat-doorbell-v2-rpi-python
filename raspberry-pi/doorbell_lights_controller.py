2b6719bf909cce7361e12517d65348bdc12e5790
gamename <tennis.n.smith@gmail.com>
2024-01-26 22:47:28 -0600

    update for APA102-2020

--- raspberry-pi/doorbell_lights_controller.py
+++ raspberry-pi/doorbell_lights_controller.py
@@ -1,5 +1,5 @@
 """
-DoorBellLightsController Class
+DoorBellLightsController Class for APA102-2020 LED Strip
 
 This class allows you to control lights based on a dark indicator sensor using a motion sensor or other sensor.
 It manages turning on and off the lights based on the darkness detected by the sensor.
@@ -15,82 +15,72 @@ import sys
 import time
 
 import RPi.GPIO as GPIO
-# Import necessary libraries
+# Import necessary libraries for APA102
+import adafruit_dotstar
 import board
-import neopixel
 
 
 class DoorBellLightsController:
     """
-    Controller for managing lights associated with a doorbell.
-
-    This class allows you to control lights based on a dark indicator sensor using a motion sensor or other sensor.
+    Controller for managing APA102-2020 LED strip lights associated with a doorbell.
 
     Args:
         dark_indicator_pin (int): The GPIO pin number connected to the dark indicator sensor.
-        led_strip_pin (board.Pin): The pin used to control the LED strip.
+        ci_pin (board.Pin): Clock Interface pin for the APA102 LEDs.
+        di_pin (board.Pin): Data Interface pin for the APA102 LEDs.
         logger (logging.Logger, optional): The logger to use for logging events. Defaults to None.
-        max_pixels (int, optional): The maximum number of pixels in the LED strip. Defaults to 4.
+        max_pixels (int, optional): The maximum number of pixels in the LED strip. Defaults to 20.
 
     Attributes:
         dark_indicator_pin (int): The GPIO pin number connected to the dark indicator sensor.
-        led_strip_pin (board.Pin): The pin used to control the LED strip.
+        ci_pin (board.Pin): Clock Interface pin for the APA102 LEDs.
+        di_pin (board.Pin): Data Interface pin for the APA102 LEDs.
         max_pixels (int): The maximum number of pixels in the LED strip.
         logger (logging.Logger or None): The logger used for logging events.
 
     Methods:
         _is_dark(): Check if it's dark based on the dark indicator sensor.
         turn_on(): Turn on the lights if it's dark and log the event.
-        turn_off(): Turn off the lights if it's dark and log the event.
+        turn_off(): Turn off the lights.
     """
     LIGHTS_ON = (255, 255, 255)  # White
     LIGHTS_OFF = (0, 0, 0)  # Black
 
-    def __init__(self, dark_indicator_pin, led_strip_pin, logger=None, max_pixels=4):
+    def __init__(self, dark_indicator_pin, ci_pin, di_pin, logger=None, max_pixels=20):
         self.dark_indicator_pin = dark_indicator_pin
-        self.led_strip_pin = led_strip_pin
+        self.ci_pin = ci_pin
+        self.di_pin = di_pin
         self.max_pixels = max_pixels
         self.logger = logger
 
-        # Initialize neopixel
-        self.pixels = neopixel.NeoPixel(self.led_strip_pin, self.max_pixels, auto_write=False)
+        # Initialize APA102 LED strip
+        self.pixels = adafruit_dotstar.DotStar(self.ci_pin, self.di_pin, self.max_pixels, brightness=0.5,
+                                               auto_write=False)
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.dark_indicator_pin, GPIO.IN)
 
     def _is_dark(self):
-        """
-        Check if it's dark based on the dark indicator sensor.
-
-        Returns:
-            bool: True if it's dark, False otherwise.
-        """
         return GPIO.input(self.dark_indicator_pin)
 
     def turn_on(self):
-        """
-        Turn on the lights if it's dark and log the event.
-        """
         if self._is_dark():
-            self.pixels.fill(DoorBellLightsController.LIGHTS_ON)  # Set pixels to ON color (white
+            self.pixels.fill(DoorBellLightsController.LIGHTS_ON)
             self.pixels.show()
             if self.logger:
                 self.logger.info("Lights turned ON")
 
     def turn_off(self):
-        """
-        Turn off the lights.
-        """
-        self.pixels.fill(DoorBellLightsController.LIGHTS_OFF)  # Set pixels to OFF color (black)
+        self.pixels.fill(DoorBellLightsController.LIGHTS_OFF)
         self.pixels.show()
         if self.logger:
             self.logger.info("Lights turned OFF")
 
-
 if __name__ == "__main__":
     # Example usage with logger
     dark_pin = 2  # Physical pin 3
-    led_pin = board.D10  # Physical pin 19
+    ci_pin = board.D11  # Clock Interface pin
+    di_pin = board.D10  # Data Interface pin
 
     # Create a logger
     logger = logging.getLogger(__name__)
@@ -99,9 +89,9 @@ if __name__ == "__main__":
     handler = logging.StreamHandler(stream=sys.stdout)
     logger.addHandler(handler)
 
-    controller = DoorBellLightsController(dark_pin, led_pin, logger=logger)
+    controller = DoorBellLightsController(dark_pin, ci_pin, di_pin, logger=logger)
 
     while True:
         controller.turn_on()  # Turn on the lights if it's dark and log the event
         time.sleep(30)  # Wait for 30 seconds
-        controller.turn_off()  # Turn off the lights if it's dark and log the event
+        controller.turn_off()  # Turn off the lights
