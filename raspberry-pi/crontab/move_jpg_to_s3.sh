0004078924073a6a01e4e0523e68c4d825eaf4ec
gamename <tennis.n.smith@gmail.com>
2024-03-29 22:48:38 -0500

    move jpg files to s3

---
+++ raspberry-pi/crontab/move_jpg_to_s3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Save diagnostic jpg files to s3
+#
+
+# Directory containing the JPG files
+SOURCE_DIR="/tmp"
+
+# S3 bucket name
+S3_BUCKET="cat-doorbell-v2-jpg"
+
+# Find .jpg files and move them to the specified S3 bucket
+find $SOURCE_DIR -type f -name "*.jpg" | while read file; do
+    aws s3 mv "${file}" s3://${S3_BUCKET}/ && echo "Moved ${file} to S3"
+done
