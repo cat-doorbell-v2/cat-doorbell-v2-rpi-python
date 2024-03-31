814f8a4c47538418c72b8a7a1e4d08c2474d2e2e
gamename <tennis.n.smith@gmail.com>
2024-03-31 18:02:00 -0500

    add /usr/local/bin to path

--- raspberry-pi/crontab/move_jpg_to_s3.sh
+++ raspberry-pi/crontab/move_jpg_to_s3.sh
@@ -9,6 +9,8 @@ SOURCE_DIR="/tmp"
 # S3 bucket name
 S3_BUCKET="cat-doorbell-v2-jpg"
 
+PATH="/usr/local/bin:${PATH}"
+
 # Find .jpg files and move them to the specified S3 bucket
 find $SOURCE_DIR -type f -name "*.jpg" | while read file; do
     aws s3 mv "${file}" s3://${S3_BUCKET}/ && echo "Moved ${file} to S3"
--- raspberry-pi/crontab/move_wav_to_s3.sh
+++ raspberry-pi/crontab/move_wav_to_s3.sh
@@ -9,5 +9,7 @@ BUCKET_NAME="cat-doorbell-v3-samples"
 # Source directory for .wav files
 SOURCE_DIR="/tmp"
 
+PATH="/usr/local/bin:${PATH}"
+
 # Find and move .wav files to the S3 bucket
 find "${SOURCE_DIR}" -type f -name "*.wav" -exec aws s3 mv {} s3://${BUCKET_NAME}/ \;
