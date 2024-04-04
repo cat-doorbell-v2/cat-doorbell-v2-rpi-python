7676d5c53a3c6c1e29e9ef13a3c033e5b90f02b8
gamename <tennis.n.smith@gmail.com>
2024-04-04 11:53:06 -0500

    separate the different wav types

--- raspberry-pi/crontab/rclone-to-google-drive.sh
+++ raspberry-pi/crontab/rclone-to-google-drive.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-rclone move /tmp/ google-drive-cloning:/CatDoorbellV3/rpi-captured-files/wav --include "/*.wav" --verbose
+rclone move /tmp/ google-drive-cloning:/CatDoorbellV3/rpi-captured-files/wav/meow --include "/meow*.wav" --verbose
+
+rclone move /tmp/ google-drive-cloning:/CatDoorbellV3/rpi-captured-files/wav/unknown --include "/unknown*.wav" --verbose
 
 rclone move /tmp/ google-drive-cloning:/CatDoorbellV3/rpi-captured-files/jpg --include "/*.jpg" --verbose
